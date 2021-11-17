5182489808cab84e77bc24b3b1e5028c02b2c7ff
Pablo Bartolomé <pablobart.me@gmail.com>
2021-11-17 08:36:47 +0100

    Function to get data from a given Excel file.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 # Save a graph given a plot and a name.
 def save_graph(plt, name):
     print(f'Image name, {name}')  # Debugging line.
     plt.savefig(f'{name}.png')
+
+
+# Get data from Excel file indicated in argument (including path if necessary).
+def get_data_Excel(file):
+    # Example line, sheet_name is optional to indicate Excel sheet to read.
+    # df = pd.read_excel(r'Path where the Excel file is stored\File name.xlsx', sheet_name='your Excel sheet name')
+
+    df = pd.read_excel(r'file')
+    print(df) # Print data for debugging purposes.
 
 
 # Press the green button in the gutter to run the script.
