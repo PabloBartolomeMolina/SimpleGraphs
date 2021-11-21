a6566eb086f946712a781c53245ea5533b86d371
Pablo Bartolomé <pablobart.me@gmail.com>
2021-11-21 21:20:46 +0100

    Check file existance. Dummy DataFrame is created for basic management. To improve behavior for the usecase.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import os.path
 
 
 # Save a graph given a plot and a name.
@@ -15,8 +16,16 @@ def get_data_excel(file, sheet = 0):
     # Example line, sheet_name is optional to indicate Excel sheet to read.
     # df = pd.read_excel(r'Path where the Excel file is stored\File name.xlsx', sheet_name='your Excel sheet name')
 
-    df = pd.read_excel(f'{file}', sheet)
-    print(df) # Print data for debugging purposes.
+    # Check if file is existing.
+    if os.path.isfile(file):
+        df = pd.read_excel(f'{file}', sheet)
+        print(df) # Print data for debugging purposes.
+    else:
+        # As file do not exist, create a dummy DataFrame.
+        # Useful to debug. Specific implementation to manage this error, TO BE DONE.
+        data = {'NUM0': [0, 0, 0, 0], 'NUM1': [1, 1, 1, 1]}
+        df = pd.DataFrame(data)
+
     return df
 
 
