7fd06bb29a60a0f671c4ccd4975a8e1cbeddd717
Pablo Bartolomé <pablobart.me@gmail.com>
2021-11-30 20:51:58 +0100

    Reformat of document: separation between functions and debug messages

--- reportGenerator.py
+++ reportGenerator.py
@@ -13,13 +13,14 @@ def create_document():
     document.add_heading('Demo heading, level 1', level=1)
     document.save('demo.docx')
 
+
 # Open a given document if it exists.
 def open_document(filename):
     try:
         f = open(f'{filename}.docx', 'rb')
         document = Document(f)
-        print("Document is open")
+        print("Document: "f'{filename}.docx' " is open")
         f.close()
     except FileNotFoundError:
-        print("Document not found")
+        print("\nDocument not found:\n" f'{filename}.docx')
 
